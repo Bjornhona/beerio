6478971369bb18c0fca7b51cc3edc415176577f4
Bjornhona <asaeri3@hotmail.com>
2021-01-22 06:58:03 +0100

    cashed backend project removed

---
+++ beerio-backend/keys.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//   mongoUsername: process.env.MONGO_INITDB_ROOT_USERNAME,
+//   // MONGO_INITDB_ROOT_USERNAME=mongoadmin,
+//   mongoPassword: process.env.MONGO_INITDB_ROOT_PASSWORD,
+//   // MONGO_INITDB_ROOT_PASSWORD=secret
+//   // MONGO_INITDB_DATABASE: <database to create>,
+//   {/* port: process.env.DB_PORT,
+//   host: process.env.DB_HOST,
+//   name: process.env.DB_NAME */}
+// }
